52f009310c41291913423ac9f3c1d75ccca4b123
Lucas Sobrinho <lucas.ns.93@hotmail.com>
2025-06-20 11:06:57 -0300

    fix

--- Exercicios/Exercicio5/Application/Saldo/Queries/GetSaldoByContaCorrenteQuery.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-
-namespace Questao5.Application.Saldo.Queries
-{
-    public class GetSaldoByContaCorrenteQuery : IRequest<SaldoResponse>
-    {
-        public string IdContaCorrente { get; set; }
-    }
-}
